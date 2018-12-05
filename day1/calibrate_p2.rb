046db95d4859dee08d42d5d48c8c3a52be51566a
Lee Saville <lee.saville@americanfinancing.net>
2018-12-05 08:07:30 -0700

    Clean up slightly

--- day1/calibrate_p2.rb
+++ day1/calibrate_p2.rb
@@ -4,11 +4,12 @@ sum = 0
 seen = {
   0 => 'seen'
 }
-twice = nil
 
-twice = input.split(/\n/).cycle do |num|
+input.split(/\n/).cycle do |num|
   num = num.to_i
   sum += num
-  break twice = sum if seen[sum]
+  break if seen[sum]
   seen[sum] = 'seen'
 end
+
+puts sum
