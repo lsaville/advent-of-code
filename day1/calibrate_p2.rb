1e17aa1ee11af18bb8bbcea30951ba5a333b4e68
Lee Saville <lee.saville@americanfinancing.net>
2018-12-05 08:03:39 -0700

    Remove if statement

--- day1/calibrate_p2.rb
+++ day1/calibrate_p2.rb
@@ -1,4 +1,3 @@
-require 'pry'
 input = File.read('input.txt')
 
 sum = 0
@@ -7,14 +6,9 @@ seen = {
 }
 twice = nil
 
-input.split(/\n/).cycle do |num|
+twice = input.split(/\n/).cycle do |num|
   num = num.to_i
   sum += num
-  if seen[sum]
-    binding.pry
-    return twice = sum
-  else
-    seen[sum] = 'seen'
-  end
+  break twice = sum if seen[sum]
+  seen[sum] = 'seen'
 end
-binding.pry
